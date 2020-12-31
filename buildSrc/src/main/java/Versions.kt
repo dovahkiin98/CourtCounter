a6081f0377696687f96e0a3292956a130b9243b0
ahmad <ezio1497@gmail.com>
2020-12-31 06:54:05 +0200

    Updated Dependencies

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -12,10 +12,10 @@ object Versions {
     const val fragment = "1.3.0-rc01"
     const val lifecycle = "2.3.0-rc01"
 
-    const val constraintLayout = "2.1.0-alpha1"
+    const val constraintLayout = "2.1.0-alpha2"
 
     const val compose = "1.0.0-alpha09"
 
     // Google
-    const val material = "1.3.0-alpha04"
+    const val material = "1.3.0-beta01"
 }
